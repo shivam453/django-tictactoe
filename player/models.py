bb43d2814ca8b92a276a7438334220ed4f070e73
Shivam <goelshivam453@gmail.com>
2019-07-23 00:59:05 +0530

    add files

--- player/models.py
+++ player/models.py
@@ -3,7 +3,16 @@ from django.contrib.auth.models import User
 
 
 class Invitation(models.Model):
-    from_user = models.ForeignKey(User, related_name="invitations_sent")
-    to_user = models.ForeignKey(User, related_name="invitations_received")
-    message = models.CharField(max_length=300)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    from_user = models.ForeignKey(User, related_name="invitations_sent",
+                                  on_delete=models.CASCADE)
+    to_user = models.ForeignKey(
+        User,
+        related_name="invitations_received",
+        verbose_name="User to invite",
+        help_text="Please select a user you want to play a game with.",
+    )
+    message = models.CharField(
+        max_length=300, blank=True,
+        verbose_name="Optional Message",
+        help_text="It's always nice to add a friendly message!"
+    )
--- player/urls.py
+++ player/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import home, new_invitation
+from .views import home, new_invitation, accept_invitation, SignUpView
 
 urlpatterns = [
     url(r'home$', home, name='player_home'),
@@ -10,5 +10,11 @@ urlpatterns = [
     url(r'logout$',
         LogoutView.as_view(),
         name="player_logout"),
-    url(r'new_invitation$', new_invitation, name='player_new_invitation')
+    url(r'new_invitation$', new_invitation, name='player_new_invitation'),
+    # https://docs.python.org/3/howto/regex.html
+    # Syntax: (?P<name>expr)
+    url(r'accept_invitation/(?P<id>\d+)/$',
+        accept_invitation,
+        name="player_accept_invitation"),
+    url(r'signup$', SignUpView.as_view(), name='player_signup'),
 ]
